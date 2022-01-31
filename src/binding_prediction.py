eb1865312e57ade401f0dc29db03c8c02d0ee513
Lanorius <Danielsg@web.de>
2022-01-31 10:39:21 +0100

    changed file structure and added files

--- src/binding_prediction.py
+++ src/binding_prediction.py
@@ -52,12 +52,15 @@ elif dummy_run and overtrain:
 else:
     number_of_random_draws = 2
 
-batch_sizes = list(range(128, 257, 4))
+# batch_sizes = list(range(128, 257, 4))
+batch_sizes = list(range(256, 513, 4))
 learning_rates = [0.01, 0.001, 0.0001]
+
 #  learning_rates = list(np.arange(0.0001, 0.01, 0.0001))
 
 if not dummy_run:
-    numbers_of_epochs = list(range(200, 401))
+    # numbers_of_epochs = list(range(200, 401))
+    number_of_epochs = list(range(300, 501))
 else:
     numbers_of_epochs = list(range(3, 6))
 
