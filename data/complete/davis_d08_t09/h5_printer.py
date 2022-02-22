587a330a79ccb1b8c587a4b16fd22ccd4a0d5873
Daniel <Danielsg@web.de>
2022-02-22 12:15:07 +0100

    getting some results

--- data/complete/davis_d08_t09/h5_printer.py
+++ data/complete/davis_d08_t09/h5_printer.py
@@ -2,7 +2,6 @@ from silx.io.dictdump import h5todict
 
 file = "smiles_embeddings_as_hd5.h5"
 in_file = h5todict(file)
-print(in_file)
+print(len(in_file))
 
-#print(type(in_file[in_file[0]]))
-print(len(in_file['11717001']))
+
